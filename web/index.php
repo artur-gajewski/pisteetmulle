e3a0467957d5a3c390fcc16e2496aa4c04e61bb9
Artur Gajewski <artur.gajewski@fraktio.fi>
2014-09-10 21:13:43 +0300

    Use database instead of files

--- web/index.php
+++ web/index.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once __DIR__.'/../vendor/autoload.php';
+require_once __DIR__.'/../src/config/database.php';
 
 use Symfony\Component\HttpFoundation\Request;
 
@@ -8,13 +9,19 @@ $app = new Silex\Application();
 
 $app['debug'] = true;
 
+// Database
+$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
+    'db.options' => $databaseConfigs,
+));
+
 // Twig
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/../src/views',
 ));
 
 // Truncate filter for Twig
-$app['twig']->addFilter(new Twig_SimpleFilter('truncate', function($string, $size) {
+$app['twig']->addFilter(new Twig_SimpleFilter('truncate', function($string, $size)
+{
     if(strlen($string) < $size) {
         return $string;
     } else {
@@ -23,41 +30,80 @@ $app['twig']->addFilter(new Twig_SimpleFilter('truncate', function($string, $siz
 }));
 
 // Root page with form
-$app->get('/', function () use ($app) {
+$app->get('/', function () use ($app)
+{
     return $app['twig']->render('form.twig', array(
     ));
 });
 
 // Save the form's data
-$app->post('/', function (Request $request) use ($app) {
+$app->post('/', function (Request $request) use ($app)
+{
     $story = $request->get('story');
     $author = $request->get('author');
-    $date = date('d.n.Y');
 
-    $id = base_convert(rand(100, 999) . microtime(true), 10, 36);
-    $data = $story . '|' . $author . '|' . $date;
+    $shortUrl = base_convert(rand(100, 999) . microtime(true), 10, 36);
 
-    file_put_contents(__DIR__.'/../data/' . $id . '.txt', $data);
+    $app['db']->insert('entries', array(
+        'shorturl' => $shortUrl,
+        'author'   => $author,
+        'story'    => $story,
+    ));
 
-    return $app->redirect('/' . $id);
+    return $app->redirect('/' . $shortUrl);
+});
+
+// convert to database
+$app->get('/convert', function () use ($app)
+{
+    $files = scandir(__DIR__.'/../data');
+
+    foreach( $files as $fl) {
+
+        if ($fl != '.' && $fl != '..') {
+
+            $data = file_get_contents(__DIR__.'/../data/' . $fl);
+
+            $fl = str_replace('.txt', '', $fl);
+            $shortUrl = $fl;
+
+            $entry = $app['db']->fetchAssoc('SELECT * FROM entries WHERE shorturl = :shorturl', array(
+                'shorturl' => $shortUrl,
+            ));
+
+            if (!$entry) {
+
+                $row = explode('|', $data);
+
+                if (sizeof($row) == 3) {
+                    list($story, $author, $created) = explode('|', $data);
+
+                    $app['db']->insert('entries', array(
+                        'shorturl' => $shortUrl,
+                        'author'   => $author,
+                        'story'    => $story,
+                    ));
+                }
+            }
+        }
+    }
 });
 
 // View saved data
-$app->get('/{id}', function ($id) use ($app) {
-    $fileName = __DIR__.'/../data/' . $id . '.txt';
+$app->get('/{shortUrl}', function ($shortUrl) use ($app)
+{
+    $entry = $app['db']->fetchAssoc('SELECT * FROM entries WHERE shorturl = :shorturl', array(
+        'shorturl' => $shortUrl,
+    ));
 
-    if (!file_exists($fileName)) {
+    if (!$entry) {
         return $app->redirect('/');
     }
 
-    $data = file_get_contents($fileName);
-
-    list($story, $author, $date) = explode('|', $data);
-
     return $app['twig']->render('view.twig', array(
-        'story'  => $story,
-        'author' => $author,
-        'date'   => $date
+        'story'   => $entry['story'],
+        'author'  => $entry['author'],
+        'created' => $entry['created'],
     ));
 });
 
